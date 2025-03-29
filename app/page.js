106849bc797c8ef210f7ff2c81214348a4ce9bec
Alexandre Rozante <alexrozante@hotmail.com>
2025-03-29 00:35:58 -0300

    ate aula 105 (NavLink - client component)

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Link from 'next/link';
-
 import classes from './page.module.css';
+import Slideshow from '@/components/image-slideshow';
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <Slideshow/>
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
--- components/main-header.js
+++ components/main-header.js
@@ -1,7 +1,8 @@
-import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css"
+import Link from "next/link";
+import NavLink from "@/components/nav-link";
 
 export default function MainHeader({params}) {
 
@@ -22,10 +23,10 @@ export default function MainHeader({params}) {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href='/meals'>Browse Meals</Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href='/community'>Foodies Community</Link>
+              <NavLink href='/community'>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
