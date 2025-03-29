5466707137754f48e276847ea3b4ade052d79730
Alexandre Rozante <alexrozante@hotmail.com>
2025-03-29 20:10:21 -0300

    ate aula 111 - loading page v1

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import classes from './meal-item.module.css';
 
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,8 +1,9 @@
 import classes from './meals-grid.module.css';
+import MealItem from './meal-item';
 
 export default function MealsGrid({meals}) {
   return (
-    <ul className={classes.meal}>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal}/>
