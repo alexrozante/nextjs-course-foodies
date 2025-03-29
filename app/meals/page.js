01663ad1c860c7ac377743037b8fc00b12cf76e9
Alexandre Rozante <alexrozante@hotmail.com>
2025-03-29 19:54:22 -0300

    ate aula 108 (meals pages sem BD)

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,20 @@
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals created <span className={classes.highlight}>by you</span></h1>
+        <p>Chosse your favorite recipe and cook it youtself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/mease/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
